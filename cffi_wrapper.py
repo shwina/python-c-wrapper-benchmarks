4aceb62afceb5fc75b7fd0c063b4be8cd3fe395f
Ashwin Srinath <shwina@users.noreply.github.com>
2018-10-03 08:27:33 -0400

    Add script to generate cffi wrapper

---
+++ cffi_wrapper.py
@@ -0,0 +1,18 @@
+from cffi import FFI
+ffibuilder = FFI()
+
+# cdef() expects a string listing the C types, functions and
+# globals needed from Python. The string follows the C syntax.
+ffibuilder.cdef("""
+    void solve(double* A, double* b, int n);
+""")
+
+# This describes the extension module "_pi_cffi" to produce.
+ffibuilder.set_source("_libsolve_cffi",
+"""
+     #include "libsolve/solve.h"   // the C header of the library
+""",
+     libraries=['libsolve/libsolve.so'])   # library name, for the linker
+
+if __name__ == "__main__":
+    ffibuilder.compile(verbose=True)
